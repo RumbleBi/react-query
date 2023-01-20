a8d3bdf84f8ab8b8289b620db9acbe9a34d40e5c
RumbleBi <olzlel2000@gmail.com>
2023-01-20 18:06:52 +0900

    react-query pagination practice

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,29 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+
 async function fetchComments(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
   return response.json();
 }
 
 async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, {
+    method: "DELETE",
+  });
   return response.json();
 }
 
 async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, {
+    method: "PATCH",
+    data: { title: "REACT QUERY FOREVER!!!!" },
+  });
   return response.json();
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery(["comments", post.id], () =>
+    fetchComments(post.id)
+  );
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  if (isError) {
+    return (
+      <>
+        <h3>Error</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
