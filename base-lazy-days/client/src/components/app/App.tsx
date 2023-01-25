85d7a52a9ccf64d4d6ab06d7c2ece81380447ed1
RumbleBi <olzlel2000@gmail.com>
2023-01-25 20:31:38 +0900

    project_init

--- base-lazy-days/client/src/components/app/App.tsx
+++ base-lazy-days/client/src/components/app/App.tsx
@@ -1,17 +1,23 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactElement } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import { theme } from '../../theme';
 import { Loading } from './Loading';
 import { Navbar } from './Navbar';
+import { queryClient } from '../../react-query/queryClient';
 import { Routes } from './Routes';
+import { theme } from '../../theme';
 
 export function App(): ReactElement {
   return (
     <ChakraProvider theme={theme}>
-      <Navbar />
-      <Loading />
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Loading />
+        <Routes />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
